a56ae0594de8d282341771a201361da52b4807c8
Max Horstmann <max.horstmann@gmail.com>
2020-12-19 17:05:43 -0500

    new index

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const Controller = {
     ev.preventDefault();
     const form = document.getElementById("form");
     const data = Object.fromEntries(new FormData(form));
-    const response = fetch(`http://shakesearch.eastus.azurecontainer.io:9200/completeworks/_search?q=${data.query}`, { mode: 'cors'}).then((response) => {
+    const response = fetch(`http://shakesearch.eastus.azurecontainer.io:9200/shakespeare/_search?q=${data.query}`, { mode: 'cors'}).then((response) => {
       response.json().then((results) => {
         Controller.updateTable(results);
       });
@@ -17,7 +17,7 @@ const Controller = {
     for (let hit of results.hits.hits) {
       let score = hit._score;
       let source = hit._source;
-      rows.push(`<tr>${source.content}<br>${source.title}<br>${score}</tr>`);
+      rows.push(`<tr>${source.text_entry}<br>${source.play_name}<br>${score}</tr>`);
     }
     table.innerHTML = rows;
   },
