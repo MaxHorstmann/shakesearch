d998ed9367767c056617022674d302fc39116557
Max Horstmann <max.horstmann@gmail.com>
2020-12-19 16:38:42 -0500

    render elastic results

--- app.js
+++ app.js
@@ -4,18 +4,20 @@ const Controller = {
     const form = document.getElementById("form");
     const data = Object.fromEntries(new FormData(form));
     const response = fetch(`http://shakesearch.eastus.azurecontainer.io:9200/completeworks/_search?q=${data.query}`, { mode: 'cors'}).then((response) => {
-      console.log(response);
-      // response.json().then((results) => {
-      //   Controller.updateTable(results);
-      // });
+      response.json().then((results) => {
+        Controller.updateTable(results);
+      });
     });
   },
 
   updateTable: (results) => {
+    //console.log(results);
     const table = document.getElementById("table-body");
     const rows = [];
-    for (let result of results) {
-      rows.push(`<tr>${result}<tr/>`);
+    for (let hit of results.hits.hits) {
+      let score = hit._score;
+      let source = hit._source;
+      rows.push(`<tr>${source.content}<br>${source.title}<br>${score}</tr>`);
     }
     table.innerHTML = rows;
   },
